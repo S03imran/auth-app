d99e776b0e9f06240d5bde8eddd89fc7f004d581
simran <s03imran@gmail.com>
2025-12-14 21:10:44 +0530

    Adding entitiy classes and database configuration

---
+++ auth-app/auth-app/src/main/java/com/auth/entities/Role.java
@@ -0,0 +1,23 @@
+package com.auth.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id = UUID.randomUUID();
+    @Column(unique = true, nullable = false)
+    private String name;
+}
