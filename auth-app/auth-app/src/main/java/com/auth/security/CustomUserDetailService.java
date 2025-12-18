998d9ce1f9ce1208b37c0c1e04fec3cc50d5403b
simran <s03imran@gmail.com>
2025-12-18 12:57:02 +0530

    Adding custom security config

---
+++ auth-app/auth-app/src/main/java/com/auth/security/CustomUserDetailService.java
@@ -0,0 +1,20 @@
+package com.auth.security;
+
+import com.auth.entities.User;
+import com.auth.exception.ResourceNotFoundException;
+import com.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(()-> new ResourceNotFoundException("Invalid email or password"));
+    }
+}
--- auth-app/auth-app/src/main/java/com/auth/service/AuthServiceImpl.java
+++ auth-app/auth-app/src/main/java/com/auth/service/AuthServiceImpl.java
@@ -2,14 +2,17 @@ package com.auth.service;
 
 import com.auth.dto.UserDto;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
     @Override
     public UserDto registerUser(UserDto userDto) {
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
         UserDto udto = userService.createUser(userDto);
         return udto;
     }
